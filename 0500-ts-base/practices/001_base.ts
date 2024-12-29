71617cf3bbc21b32338fc41ca5446bc22c28f699
tanakas-2021 <shugotanaka2020@gmail.com>
2024-12-29 17:02:29 +0900

    practice001がpassすることを確認

--- 0500-ts-base/practices/001_base.ts
+++ 0500-ts-base/practices/001_base.ts
@@ -10,8 +10,9 @@
    振る舞い: 引数で渡された2つの数値を足し算して返す
 */
 
-
-/* [ここに実装] */
+export const add = (num1: number, num2: number): number => {
+  return num1 + num2;
+};
 
 /* 1.2 以下の関数を実装して下さい
    関数名: sum
@@ -20,7 +21,12 @@
    振る舞い: 配列の中身の数を全て足し算して返す
 */
 
-/* [ここに実装] */
+export const sum = (nums: number[]): number => {
+  return nums.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+};
 
 /* 1.3 以下の関数を実装して下さい
    関数名: format
@@ -29,7 +35,14 @@
    振る舞い: 引数で渡されたDate型の日付をYYYY/MM/DDの形式にして返す
 */
 
-/* [ここに実装] */
+export const format = (date: Date) => {
+  const year: number = date.getFullYear();
+  const month: number = date.getMonth() + 1; //月は0始まりのため1を足す
+  const day: number = date.getDate();
+
+  const formattedDate: string = `${year}/${month < 10 ? "0" : ""}${month}/${day < 10 ? "0" : ""}${day}`;
+  return formattedDate;
+};
 
 /* 1.4 以下の関数を実装して下さい
    関数名: merge
@@ -46,7 +59,30 @@
    merge({}, {}) => {}
 */
 
-/* [ここに実装] */
+export const merge = (
+  obj1: Record<string, number>,
+  obj2: Record<string, number>
+): Record<string, number> => {
+  const result: Record<string, number> = {};
+  for (const key1 in obj1) {
+    for (const key2 in obj2) {
+      if (key1 === key2) {
+        result[key1] = obj1[key1] + obj2[key2];
+      }
+    }
+    // resultにkey1が追加されていない場合、追加する。
+    if (!result.hasOwnProperty(key1)) {
+      result[key1] = obj1[key1];
+    }
+  }
+  // resultにkey2が追加されていない場合、追加する。
+  for (const key2 in obj2) {
+    if (!result.hasOwnProperty(key2)) {
+      result[key2] = obj2[key2];
+    }
+  }
+  return result;
+};
 
 /* 1.5 以下の関数を実装して下さい
    関数名: stringify
@@ -63,4 +99,14 @@
    stringify(undefined) => "undefined"
 */
 
-/* [ここに実装] */
+export const stringify = (
+  value: string | number | boolean | null | undefined
+): string => {
+  if (value === null) {
+    return "null";
+  } else if (value === undefined) {
+    return "undefined";
+  } else {
+    return String(value);
+  }
+};
